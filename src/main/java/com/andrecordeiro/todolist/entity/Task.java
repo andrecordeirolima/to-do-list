28608958423efe252db2e84109384a37c758337a
andrecordeirolima <andrecordeirolima@gmail.com>
2023-08-03 12:24:19 -0300

    controller created

--- src/main/java/com/andrecordeiro/todolist/entity/Task.java
+++ src/main/java/com/andrecordeiro/todolist/entity/Task.java
@@ -1,7 +1,6 @@
 package com.andrecordeiro.todolist.entity;
 
 import jakarta.persistence.*;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,12 +10,49 @@ public class Task {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Getter
-    @Setter
     private Long id;
     private String name;
     private String description;
     private boolean finished;
     private int priority;
 
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isFinished() {
+        return finished;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setFinished(boolean finished) {
+        this.finished = finished;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
 }
